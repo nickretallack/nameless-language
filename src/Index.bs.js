38696052699bec1ac333a39748efff7e4b033d82
Nick Retallack <nickretallack@gmail.com>
2019-12-05 00:47:04 -0800

    removed bs.js files

--- src/Index.bs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var React = require("react");
-var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
-var AppView$ReasonReactExamples = require("./AppView.bs.js");
-
-ReactDOMRe.renderToElementWithId(React.createElement(AppView$ReasonReactExamples.make, { }), "app");
-
-/*  Not a pure module */
